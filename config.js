22db199653c43db5b0907aad44ecf1787065f5f0
Tomate <tronsin7@gmail.com>
2024-04-02 12:18:22 +0200

    add wasabi settings

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   s3: {
-    accessKeyId: '', // TODO
-    secretAccessKey: '', // TODO
-    endpoint: '', // TODO
-    bucket: '', // TODO
+    accessKeyId: 'XLX5EW77QAVH5L6RPM3H', // TODO
+    secretAccessKey: 'NWAJusCWborUdBc9hTNIABjGKF46Ohmaf4mjsIRh', // TODO
+    endpoint: 's3.eu-west-2.wasabisys.com', // TODO
+    bucket: ' esgi-cloud-exam-bucket-75032', // TODO
   },
   suffix: {
     small: '_small.jpg',
